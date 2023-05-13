d1b558ad38262e7bcc0c94ecd5f46102d108233b
Vanaja Sangati <vanaja1073@gmail.com>
2023-05-13 16:10:27 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading =React.createElement("div",{},React.createElement("div",{},React.createElement("h1",{id:"heading"},"hello")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
